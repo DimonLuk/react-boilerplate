70c0d0c24e9a2142a256ec981363599152e1e7f8
Stanislav Sorokin <ssorokin@equityeffect.com>
2020-03-07 15:03:30 +0200

    sentry-setup: add initial sentry setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+import * as Sentry from "@sentry/browser";
+if (process.env.REACT_APP_SENTRY_URL) {
+  Sentry.init({ dsn: process.env.REACT_APP_SENTRY_URL });
+}
 function App() {
   return (
     <div className="App">
